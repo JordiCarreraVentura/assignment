cb0baa37d3a02859a1fb045305f1daf242b9cc79
Jordi Carrera Ventura <jordi@immention.com>
2020-08-27 15:30:52 +0200

    Allow to read arbitrary input from a hard-coded file

--- Clusters.py
+++ Clusters.py
@@ -24,6 +24,7 @@ from sklearn.feature_extraction.text import (
 )
 
 from tools import (
+    from_json,
     to_json
 )
 
@@ -238,6 +239,10 @@ if __name__ == '__main__':
     d = TitleTextDataset()
     
     X, Y = list(zip(*d))
+    X = [
+        '%s. %s' % (o['title'], o['text'])
+        for o in from_json('data/clusters.json')
+    ]
     
     #X = random.sample(X, int(len(X) * 0.2))
     
