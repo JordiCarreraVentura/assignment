4854d14250bcb5cac452d5022277f10856618178
Jordi Carrera Ventura <jordi@immention.com>
2020-08-28 20:26:11 +0200

    Simplify output

--- cli.py
+++ cli.py
@@ -10,7 +10,6 @@ class DeserializedModelWrapper:
 
     def __init__(self, payload):
         self._payload = payload
-        print(type(self._payload))
         if isinstance(self._payload, GridSearchCV):
             self._predictor = self._payload.best_estimator_.predict
         else:
@@ -27,7 +26,7 @@ class DeserializedModelWrapper:
 if __name__ == '__main__':
     
     model_path = 'models/ngrams.TitleTextDataset.svm_ngrams.p'
-    model_path = 'models/sentence_transformer__sentence_encoders.p'
+    #model_path = 'models/sentence_transformer__sentence_encoders.p'
     
     model = DeserializedModelWrapper(from_pickle(model_path))
     
@@ -38,10 +37,7 @@ if __name__ == '__main__':
     
     Y = model(X)
     
-    for x, y in zip(X, Y):
-        print('%s...' % x[:250])
-        print('predicted=%d expected=%d\n' % (y, 1))
-    
+    print('Accuracy: %.2f' % (sum(Y) / float(len(Y))))
     
     
     
